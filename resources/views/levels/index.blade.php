cfd5c74a62b866c6ad6c1666e5582834e3614d8f
Sangram Wadhaval <swadhaval24@gmail.com>
2022-11-29 22:29:36 +0530

    added add functionality in frontend

--- resources/views/levels/index.blade.php
+++ resources/views/levels/index.blade.php
@@ -8,12 +8,33 @@
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
   <title>Levels</title>
 </head>
 <body>
   <div class="container">
     <h1>Levels</h1>
+    <div style="cursor: pointer" class="open-add-modal-btn">
+      <i class="bi bi-plus-circle"></i>
+    </div>
     <div id="levels-container"></div>
+  </div>
+
+  {{-- add level modal  --}}
+  <div class="modal fade" id="level-add-modal">
+    <div class="modal-dialog modal-dialog-centered">
+       <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5">Add Level</h1>
+        </div>
+        <div class="modal-body">
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" onclick="toggleAddLevelModal()">Close</button>
+          <button type="button" class="btn btn-primary" id="submit-add-level-btn">Add</button>
+        </div>
+      </div>
+    </div>
   </div>
 
   <script>
@@ -31,15 +52,61 @@
         if(levelA.connectedWith == levelB.connectedWith) return 0;
         if(levelB.connectedWith == null && levelA.connectedWith < levelB.id ) return -1;
       });
-      const html = levels.reduce((html, {label, connectedWith}) => {
+      const html = levels.reduce((html, {id, label, connectedWith}) => {
         return html + (
           `<div class='mt-1'>`+
-            `<div style='margin-left: ${connectedWith ? 30 : 0}px'>${label}</div>` +
+            `<div class='d-inline' style='margin-left: ${connectedWith ? 30 : 0}px'>${label}</div>` +
+            (!connectedWith ? (
+              `<i 
+              style="cursor: pointer" 
+              class="bi bi-plus-circle open-add-modal-btn" 
+              data-connected-with='${id}'
+              ></i>
+              `
+            ) : ``) +
           `</div>`
         );
       }, '')
       $('#levels-container').html(html);
+
+      addListerenersToLevelContainer();
     }
+
+    function addListerenersToLevelContainer(){
+      $('.open-add-modal-btn').on('click', (e) => {
+        const connectedWith = $(e.target).data('connected-with');
+        $('#level-add-modal .modal-body').html(
+          `<input id='input-add-level' class='w-100' data-connected-with='${connectedWith ? connectedWith : ""}' />`
+        );
+        toggleAddLevelModal();
+      });
+    }
+
+    function toggleAddLevelModal(){
+      $('#level-add-modal').modal('toggle');
+    }
+
+
+    $('#submit-add-level-btn').on('click', () => {
+      const connectedWith = $('#input-add-level').data('connected-with');
+      const label = $('#input-add-level').val();
+
+      $.ajax({
+        type: 'POST',
+        url: `/api/levels/store`,
+        data: new URLSearchParams({
+          label,
+          connectedWith
+        }),
+        success: (data) => {
+          renderLevelContainer(data.data.levels);
+          toggleAddLevelModal();
+        },
+        processData: false
+      })
+    });
+
+
   </script>
 </body>
 </html>
